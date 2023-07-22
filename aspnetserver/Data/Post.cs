9fc85c1ce8033eef6c4fddb019ababacef91191a
JamesLian7 <jameslian1104@gmail.com>
2023-07-22 14:16:16 +1200

    lkjhh

--- aspnetserver/Data/Post.cs
+++ aspnetserver/Data/Post.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace aspnetserver.Data
 {
-	public class Post
-	{
-		public Post()
-		{
-		}
-	}
-}
+    internal sealed class Post
+    {
+        [Key]
+        public int PostId { get; set; }
 
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100000)]
+        public string Content { get; set; } = string.Empty;
+    }
+}
