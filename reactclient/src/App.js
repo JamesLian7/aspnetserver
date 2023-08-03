ee73c218b165febad12ce28adebcb7df328fb4d4
JamesLian7 <jameslian1104@gmail.com>
2023-08-03 13:38:12 +1200

    pre-disaster

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -8,6 +8,7 @@ import ResponsiveAppBar from "./components/NavBar";
 
 
 
+
 export default function App() {
   const [posts, setPosts] = useState([]);
   const [showingCreateNewPostForm, setShowingCreateNewPostForm] = useState(false);
@@ -53,7 +54,7 @@ export default function App() {
         <div className="col d-flex flex-column justify-content-center align-items-center">
           {(showingCreateNewPostForm === false && postCurrentlyBeingUpdated === null) && (
             <div>
-              <h1>Pickup Football</h1>
+              <h1>Pickup FootballFinder</h1>
 
               <div className="mt-5">
                 <button onClick={getPosts} className="btn btn-dark btn-lg w-100">Get Posts from server</button>
